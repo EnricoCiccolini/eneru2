d792366bffc30bbe74b712d9660d5bea360fde18
EnricoCiccolini <piroroma90@gmail.com>
2025-04-22 19:53:22 +0200

    feat : clone progect

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import NavBar from './component/NavBar';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Games from './pages/Games'
+import LastCard from './pages/LastCard';
+import About from './pages/About';
+import BuriedCard from './pages/BuriedCard';
+import ThirdCard from './pages/ThirdCard';
+
+export default function App() {
+
+  return (
+    <>
+
+      <BrowserRouter >
+        <Routes>
+          <Route element={<NavBar />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/games" >
+              <Route index element={<Games />} />
+              <Route path="ultimo/:slug" element={<LastCard />} />
+              <Route path="sepolti/:slug" element={<BuriedCard />} />
+              <Route path="horror/:slug" element={<ThirdCard />} />
+            </Route >
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<h1>pagina non trovata </h1>} />
+          </Route>
+
+        </Routes>
+
+
+      </BrowserRouter>
+    </>
+  )
+};
+
+
+
+
